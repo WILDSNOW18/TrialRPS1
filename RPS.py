a5d48358779fb64b551023f750b2156c186f836c
WILDSNOW18 <kcsmith972@outlook.com>
2024-10-06 13:53:26 -0700

    Create RPS.py

---
+++ RPS.py
@@ -0,0 +1,16 @@
+import random
+
+
+compsChoice = ""
+
+compsNum = random.randint(1,3)
+
+if compsChoice == 1:
+    compsChoice = "Rock"
+    
+elif compsNum == 2:
+    compsChoice = "Paper"
+
+else:
+    compsChoice = "Siccors"
+
